c119bad8fa79030fa4acf170e58a6b041f456807
Igor miyada <igormiyada@gmail.com>
2025-12-17 11:09:51 -0300

    Fixing : InputMismatch treatment

--- br/com/alura/screenmatch/main/MainApp.java
+++ br/com/alura/screenmatch/main/MainApp.java
@@ -2,22 +2,26 @@ package br.com.alura.screenmatch.main;
 
 import br.com.alura.screenmatch.model.Menu;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MainApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Menu menu = new Menu();
-
         int number = 0;
         while (number!=8){
             System.out.println(menu);
-            System.out.println("Enter the option number : ");
-            number = sc.nextInt();
             try{
+                System.out.println("Enter the option number : ");
+                number = sc.nextInt();
+                sc.nextLine();
                 menu.opcoesMenu(number);
                 Thread.sleep(2000);
-            }catch (InterruptedException e){
+            }catch (InputMismatchException e) {
+                System.err.println("Please, enter a number");
+                sc.nextLine();
+            } catch (InterruptedException e){
                 System.err.println(e.getMessage());
             }
 
